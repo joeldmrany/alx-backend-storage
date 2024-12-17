ff25206988140df4afc32bcd59e20b19c7dcd291
joeldmrany <joeldmrany1@gmail.com>
2024-12-17 14:37:40 +0200

    task

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''
+starting with redis-server
+'''
+
+import redis
+import uuid
+
+
+class Cache:
+    ''' very important class '''
+    def __init__(self):
+        ''' init you now it '''
+        self._redis = redis.Redis()
+
+    def store(self, data):
+        ''' storing data '''
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
